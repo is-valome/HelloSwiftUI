aaa8545d5f238255ca9522b3bcaeac2785540ccd
valome <vchimerem123@gmail.com>
2025-05-22 16:01:49 +0000

    Initial commit

---
+++ MyFirstXcodeProject/MyFirstXcodeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstXcodeProjectApp.swift
+//  MyFirstXcodeProject
+//
+//  Created by valome on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstXcodeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
